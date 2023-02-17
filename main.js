a1993b71df9dcfee3e681e71a5ef8b817a7d8521
Peter Grajcar <petergrajcar@duck.com>
2023-02-17 10:41:21 +0100

    initial commit
    Signed-off-by: Peter Grajcar <petergrajcar@duck.com>

---
+++ main.js
@@ -0,0 +1,55 @@
+
+const main = document.getElementById("main")
+
+fetch("http://www.ms.mff.cuni.cz/~grajcarp/mnt/news.json", {method: 'get', mode: 'cors'})
+    .then((response) => response.text())
+    .then((text) => {
+        const json = '{ "data": ' + text + '}'
+        const data = JSON.parse(json)["data"]
+
+        main.innerHTML = ""
+
+        for (x of data) {
+            if (!x["new_content"])
+                continue
+
+            const divider = document.createElement("div")
+            divider.classList.add("divider")
+            const article = document.createElement("div")
+            article.classList.add("article")
+            const header = document.createElement("div", { "class": "" })
+            header.classList.add("article-header")
+            const body = document.createElement("div")
+            body.classList.add("body")
+            const time = document.createElement("span")
+            time.classList.add("article-time")
+            const link = document.createElement("a")
+            link.classList.add("article-link-to-original")
+            link.setAttribute("href", x["link"])
+            const paragraph = document.createElement("p")
+
+            time.innerText = x["time"]
+            link.innerText = "pôvodná správa"
+            paragraph.innerHTML = x["new_content"]
+            divider.innerHTML = "&#9672; &#9672; &#9672;"
+
+
+            link.addEventListener("click", (e) => {
+                if (!confirm("Počuj, priateľu, tento odkaz ťa môže vniesť do hlbín mora negativity. Si si istý, že chceš podstúpiť toto riziko?"))
+                    e.preventDefault()
+            })
+
+            header.appendChild(time)
+            header.appendChild(link)
+            body.appendChild(paragraph)
+            article.appendChild(header)
+            article.appendChild(body)
+            main.appendChild(article)
+            main.appendChild(divider)
+        }
+    })
+    .catch((error) => {
+        main.innerText = "Z ľútosťou Vám oznamujeme, že minúta pomunila. Skúste to prosím neskôr."
+        console.error(error)
+    })
+
---
+++ scraper/mnt.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+import json
+import re
+from tqdm import tqdm
+from chatgpt_wrapper import ChatGPT
+from selenium import webdriver
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.webdriver.common.by import By
+
+template = "Svet vie byť nepekným miestom, ale teraz je čas na pozitivu a optimizmus. Prepíš následujúce, zavše trochu nelichotivé, správy na správy lásky a porozumenia. Zachovaj ich približne rovnakú dĺžku a pokús sa zachovať aj formu. Dbaj na to, nech sú dostatočne umelecké, plné nezvyčajných slovných obratov. Neboj sa popustiť uzdu svojej fantázii. Prvú vetu obklop dvoma hviezdičkami **.\n\n"
+
+firefox_dev_binary = FirefoxBinary("/Applications/Firefox Developer Edition.app/Contents/MacOS/firefox")
+options = webdriver.FirefoxOptions()
+options.add_argument("-headless")
+browser = webdriver.Firefox(firefox_binary=firefox_dev_binary, options=options)
+
+browser.get("https://mnt.sk")
+
+with open("news.json") as fh:
+    old_data = json.load(fh)
+
+ids = set(x["id"] for x in old_data)
+
+print("Scraping articles...")
+
+data = []
+articles = browser.find_elements(By.CLASS_NAME, "mnt-FeedArticle")
+for article in tqdm(articles[:-1]):
+    toolbar = article.find_element(By.CLASS_NAME, "mnt-toolbar")
+    link_el = toolbar.find_element(By.TAG_NAME, "a")
+    time_el = link_el.find_element(By.CLASS_NAME, "mnt-time")
+    content_el = article.find_element(By.CLASS_NAME, "mnt-article").find_element(By.TAG_NAME, "p")
+
+    time = time_el.get_attribute("textContent").lstrip()
+    link = link_el.get_property("href")
+    id = link.split("/")[-1]
+
+    if id in ids:
+        continue
+
+    content = content_el.get_property("innerHTML")
+    content = re.sub("</?strong>", "**", content)
+    content = re.sub("</?a[^>]*>", "**", content)
+    content = re.sub("&nbsp;", " ", content)
+    data.append({
+        "id": id,
+        "time": time,
+        "link": link,
+        "content": content,
+    })
+
+def batch(iterable, n=1):
+    l = len(iterable)
+    for ndx in range(0, l, n):
+        yield iterable[ndx:min(ndx + n, l)]
+
+print(len(data), "new articles")
+print("Adding love, respect, and understanding...")
+
+bot = ChatGPT()
+for b in tqdm(batch(data, n=4), total=len(data) // 4 + (len(data) % 4 > 0), unit="batch"):
+    texts = "\n\n".join(article["content"] for article in b)
+    bot.new_conversation()
+    response = bot.ask(template + texts)
+    # print(template + texts)
+    # print()
+    # print(response)
+    # print("\n\n\n")
+    new_content = response.split("\n\n")[-len(b):]
+    for c, d in zip(new_content, b):
+        d["new_content"] = c
+
+with open("news.json", "w") as fh:
+    json.dump(data + old_data, fh, indent=2)
+
+browser.quit()
